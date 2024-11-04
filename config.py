b3263ae23669228b2ab4f50d452c093ad0ce54ed
Varakin-Kirill <kirilld1122@gmail.com>
2024-11-04 17:00:53 +0600

    second commit

--- config.py
+++ config.py
@@ -1,18 +1,8 @@
-BOT_TOKEN = "7024704964:AAGkL6-phYLuAMFc-p_xqKCtIDgQs3xFTc0"
-PAYMENTS_TOKEN = "381764678:TEST:90031"
+BOT_TOKEN = "TOKEN"
+PAYMENTS_TOKEN = "PAYMENTS_TOKEN"
 
-DB_NAME = "busy_beaver"
-USER = "kirill"
-PASSWORD = "bot_test"
-HOST = "postgres"
-PORT = "5432"
-
-
-# BOT_TOKEN = "5916951628:AAHrbOIyqC9rpM8MarqmRvviAjxVQbFeeHY"
-# PAYMENTS_TOKEN = "381764678:TEST:90031"
-
-# DB_NAME = "TelegramBotHH"
-# USER = "postgres"
-# PASSWORD = "admin"
-# HOST = "localhost"
-# PORT = "5432"
+DB_NAME = "DB_NAME"
+USER = "USER"
+PASSWORD = "PASSWORD"
+HOST = "HOST"
+PORT = "PORT"
